b7a26488effab28d780e153effd31ac5ab9281b2
unknown <you@example.com>
2016-05-16 13:10:49 +0200

    reducida la repeticion de codigo de la clase NewsFeed

--- NewsFeed.java
+++ NewsFeed.java
@@ -8,16 +8,14 @@ import java.util.ArrayList;
  */
 public class NewsFeed
 {
-    private ArrayList<MessagePost> messages;
-    private ArrayList<PhotoPost> photos;
+    private ArrayList<Post> messages;
 
     /**
      * Constructor for objects of class NewsFeed
      */
     public NewsFeed()
     {
-        messages = new ArrayList<>();
-        photos = new ArrayList<>();
+        messages = new ArrayList<>(); 
     }
 
     /**
@@ -25,30 +23,17 @@ public class NewsFeed
      * 
      * @message the message to introduce
      */
-    public void addMessagePost(MessagePost message)
+    public void addMessage(Post message)
     {
         messages.add(message);
-    }
+    }  
 
-    /**
-     * Add a post with a image
-     * 
-     * @message the image to introduce
-     */
-    public void addPhotoPost(PhotoPost photo)
-    {
-        photos.add(photo);
-    }
-    
     /**
      * Show the post
      */
     public void show(){
-        for(MessagePost message : messages){
+        for(Post message : messages){
             message.display();
-        }
-        for(PhotoPost photo : photos){
-            photo.display();
         }
     }
 }
