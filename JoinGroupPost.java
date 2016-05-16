161d9fe7f98c510777a1337fb12442240c770bdc
unknown <you@example.com>
2016-05-16 11:03:51 +0200

    terminada clase JoinGroupPost

---
+++ JoinGroupPost.java
@@ -0,0 +1,25 @@
+/**
+ * Write a description of class MessagePost here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class JoinGroupPost extends Post
+{
+    private String group;
+    /**
+     * Constructor for objects of class MessagePost
+     */
+    public JoinGroupPost(String user, String grupo)
+    {
+       super(user);
+       group = grupo;
+    }
+    
+    /**
+     * Metodo que devuelve el mensaje del post
+     */
+    public String getText(){
+        return "El usuario " + getUser() + " se unió a tu grupo " + group;
+    }
+}
